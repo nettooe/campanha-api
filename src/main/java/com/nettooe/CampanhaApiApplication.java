ce97bc179a7a8dd72948287c438962312e951cfd
Oliver Emanuel <netto.oe@gmail.com>
2018-05-20 15:57:12 -0300

    Ajuste versão Spring - versão anterior com bugs

--- src/main/java/com/nettooe/CampanhaApiApplication.java
+++ src/main/java/com/nettooe/CampanhaApiApplication.java
@@ -2,10 +2,9 @@ package com.nettooe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
+//@Configuration
 public class CampanhaApiApplication {
 
 	public static void main(String[] args) {
